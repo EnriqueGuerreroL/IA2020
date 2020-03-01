25ec32129d669b43387f3a60cf95b6aa04ec38cf
EnriqueGuerreroL <chikehp710@yahoo.com>
2020-03-01 14:58:02 -0600

    [ADD] Fractional part function

--- floating_point.py
+++ floating_point.py
@@ -1,8 +1,11 @@
 
 
 def get_number():
-    number = input("Ingresa el número: ")
+    number = float(input("Ingresa el número: "))
     return number
+
+def int_number(number):
+    return int(number)
 
 def dec_to_base(data, base):
     output_vector = []
@@ -12,10 +15,21 @@ def dec_to_base(data, base):
     output_vector.reverse()
     return output_vector
 
-def fractional_part():
-    pass
+def fractional_part(number):
+    # fract_pt = []
+    int_pt = []
+    while number != 0:
+        new_number = (number * 2)
+        number = new_number % 1
+        # fract_pt.append(number)
+        int_pt.append(int(new_number))
+    return int_pt
 
 if __name__ == "__main__":
-    initial_number = get_number()
-    integer_part = dec_to_base(initial_number, 2)
+    input_num = get_number()
+    int_input_num = int_number(input_num)
+    fract_input_num = input_num - int_input_num
+    fp_integer_part = dec_to_base(int_input_num, 2)
+    fp_fractional_part = fractional_part(fract_input_num)
+    
     
